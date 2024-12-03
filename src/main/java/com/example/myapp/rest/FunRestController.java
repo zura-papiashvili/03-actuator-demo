464d19c6a3244d47d52d31b87e6ffbba9b3161ba
Zura <70130285+zura-papiashvili@users.noreply.github.com>
2024-12-03 11:58:11 -0500

    feat: add controller

--- src/main/java/com/example/myapp/rest/FunRestController.java
+++ src/main/java/com/example/myapp/rest/FunRestController.java
@@ -1,10 +1,24 @@
 package com.example.myapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+    // inject properties file
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose new endpoint for "teaminfo"
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
+
     // expose "/" that return "Hello World"
     @GetMapping("/")
     public String sayHello() {
@@ -20,6 +34,6 @@ public class FunRestController {
     // expose a new endpoint for "fortune"
     @GetMapping("/fortune")
     public String getDailyFortune() {
-        return "Today is your lucky day!";
+        return "Today is your lucky day!!";
     }
 }
